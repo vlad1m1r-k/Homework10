af9a2effd30d782b7afce05cc026a3e81d973852
Vladimir <vova.kvz3@gmail.com>
2018-12-12 20:11:50 +0200

    ""

---
+++ src/ua/kiev/prog/homework10/part3/Main.java
@@ -0,0 +1,10 @@
+package ua.kiev.prog.homework10.part3;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new String("Hello World!").chars().toArray();
+        System.out.println(Arrays.toString(array));
+    }
+}
