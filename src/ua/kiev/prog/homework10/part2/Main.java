cbda4c5e7f6863732d0c8927f17ae3a7c8a4ae00
Vladimir <vova.kvz3@gmail.com>
2018-12-12 20:03:14 +0200

    ""

--- src/ua/kiev/prog/homework10/part2/Main.java
+++ src/ua/kiev/prog/homework10/part2/Main.java
@@ -1,15 +1,20 @@
 package ua.kiev.prog.homework10.part2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new Random().ints(60, -20, 20).toArray();
-        ArrayList<Integer> list = Arrays.stream(array).mapToObj(Integer::valueOf).collect(Collectors.toCollection(ArrayList::new));
-        System.out.println(list);
-        list.stream().sorted((o1, o2) -> {Math.abs(o1) - Math.abs(o2)}).collect(Sysy)
+        int[] array = new Random().ints(30, -20, 20).toArray();
+        Arrays.stream(array).mapToObj(Integer::valueOf).distinct().sorted((o1, o2) -> Math.abs(o1) - Math.abs(o2)).filter(o -> o != 0).limit(2).sorted()
+                .sorted((o1, o2) -> comparator(o1, o2)).limit(1)
+                .forEach(System.out::println);
+    }
+
+    private static int comparator(Integer o1, Integer o2) {
+        if (Math.abs(o1) == Math.abs(o2)) {
+            return -1;
+        }
+        return Math.abs(o1) - Math.abs(o2);
     }
 }
